2952200ffcb27d17411b8da881dae3c68276cba3
Silence <92719733+CM-Silence@users.noreply.github.com>
2022-05-01 18:20:58 +0800

    为新建的三个Fragment子类导入布局文件

--- app/src/main/java/com/example/myplanet/page/fragment/TimerFragment.kt
+++ app/src/main/java/com/example/myplanet/page/fragment/TimerFragment.kt
@@ -1,6 +1,11 @@
 package com.example.myplanet.page.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.myplanet.R
 
 /**
  * @ClassName TimerFragment
@@ -9,4 +14,12 @@ import androidx.fragment.app.Fragment
  * @Description
  */
 class TimerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_timer, container, false)
+    }
 }
